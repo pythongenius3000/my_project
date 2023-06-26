f96729a1c9ee61a67084ab1f2cd3295e6386c26d
Anastasia <askrapivkina@gmail.com>
2023-06-26 20:37:35 +0300

    code simplification

--- my_project.py
+++ my_project.py
@@ -1,4 +1,4 @@
-stroka = str(input('Введите строку без пробелов и в нижнем регистре: '))
+stroka = str(input('Введите строку без пробелов и в нижнeм регистре: '))
 stroka_opposite = stroka[::-1]
 if stroka == stroka_opposite:
     print(True)
