e72e4156433e61dc1bbb739a3ba0669f37eaeede
Anastasia <askrapivkina@gmail.com>
2023-07-17 13:20:35 +0300

    -

--- advertisments/app_advertisments/urls.py
+++ advertisments/app_advertisments/urls.py
@@ -3,5 +3,5 @@ from .views import index, top_sellers
 
 urlpatterns = [
     path("", index, name='main-page'), #на запрос "" получаем ответ из функции index из views
-    path("top-sellers/", top_sellers, name='top-sellers')
+    path("top-sellers", top_sellers, name='top-sellers')
 ]
