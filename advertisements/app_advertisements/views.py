4b9288f53c51e7b5de1d2b259701a4f62feca437
Anastasia <askrapivkina@gmail.com>
2023-08-22 19:49:50 +0300

    homework 10

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 from .models import Advertisements
-from .forms import AdvertisementPost
+from .forms import AdvertisementForm
+
 def index(request):
     advertisements = Advertisements.objects.all() #результат - queryset(3)
     context = {'advertisements': advertisements}
@@ -13,6 +15,15 @@ def top_sellers(request):
     return render(request, 'top-sellers.html')
 
 def advertisement_post(request):
-    form = AdvertisementPost()
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisements(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement-post.html')
+    return render(request, 'advertisement-post.html', context)
